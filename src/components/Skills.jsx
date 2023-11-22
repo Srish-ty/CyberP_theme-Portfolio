189176dd83ae3a8a1f4f64028c2fcf3bbf9a7b3c
Srishty M <srishtymangutte@gmail.com>
2023-11-22 09:09:17 +0530

    feat: added skills array in config

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,7 @@ import "./styles/Skills.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import skills from "../config/skills/skills";
 
 function Skills() {
   useEffect(() => {
@@ -12,40 +13,23 @@ function Skills() {
     <div id="skills" className="container-fluid">
       <h2 className="skillka-title topic-head">Skills</h2>
       <div className="row">
-        <div className=" col-md-4">
-          <div className="skillcard" data-aos="flip-left" data-aos-once="true">
-            <div>
-              <i class="fas fa-code"></i>
+        {skills.map((skill) => {
+          return (
+            <div className="col-md-4">
+              <div
+                className="skillcard"
+                data-aos="flip-left"
+                data-aos-once="true"
+              >
+                <div>
+                  <i className={skill.icon}></i>
+                </div>
+                <div className="skill-title">{skill.title}</div>
+                <div className="skill-content">{skill.content}</div>
+              </div>
             </div>
-            <div className="skill-title">Web Development</div>
-            <div className="skill-content">
-              HTML | CSS | JavaScript | ReactJS | Bootstrap | Node.js |
-              ExpressJS | EJS| MongoDB | MySQL | Django{" "}
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="skillcard" data-aos="flip-left" data-aos-once="true">
-            <div>
-              <i class="fab fa-android"></i>
-            </div>
-            <div className="skill-title">App Development</div>
-            <div className="skill-content">
-              Native Android Development (Java) | Flutter | Dart | XML{" "}
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="skillcard" data-aos="flip-left" data-aos-once="true">
-            <div>
-              <i class="fas fa-robot"></i>
-            </div>
-            <div className="skill-title">Machine Learning</div>
-            <div className="skill-content">
-              Python | Pandas | NumPy | Scikit-Learn | Matplotlib | Seaborn{" "}
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/config/skills/skills.js
@@ -0,0 +1,20 @@
+const skills = [
+  {
+    title: "Web Development",
+    content:
+      "HTML | CSS | JavaScript | ReactJS | Bootstrap | Node.js | ExpressJS | EJS | MongoDB | MySQL | Django",
+    icon: "fas fa-code",
+  },
+  {
+    title: "App Development",
+    content: "Native Android Development (Java) | Flutter | Dart | XML",
+    icon: "fab fa-android",
+  },
+  {
+    title: "Machine Learning",
+    content: "Python | Pandas | NumPy | Scikit-Learn | Matplotlib | Seaborn",
+    icon: "fas fa-robot",
+  },
+];
+
+export default skills;
